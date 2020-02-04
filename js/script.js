132af58806c4cab74324c4ea396a0f4ed485b9a7
Cutie on the Couch <sjpcp5@gmail.com>
2020-02-04 15:02:07 -0600

    switched to loading jquery and moment in head of HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 /* alt+shift+a gives me commented paragraphs */
+var currentdayEL = document.getElementById("#currentDay"); 
+var fieldsetEl = document.getElementsByTagName("fieldset");
+
+$(currentdayEL).append(moment().year().month().date());
+console.log(currentdayEL);
