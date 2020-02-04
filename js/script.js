a593e5d05993e0480602f8bca1644a8dfab9278a
Cutie on the Couch <sjpcp5@gmail.com>
2020-02-04 15:25:23 -0600

    added href references to github, changed placeholder value, added variables to get elements from the DOM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 /* alt+shift+a gives me commented paragraphs */
 var currentdayEL = document.getElementById("#currentDay"); 
 var fieldsetEl = document.getElementsByTagName("fieldset");
+var textareaEL = document.getElementsByTagName("textarea");
+var timeblockEl = document.getElementsByTagName("time");
+var inputbtnEl = document.getElementsByTagName("input");
 
-$(currentdayEL).append(moment().year().month().date());
+$(currentdayEL).append(moment().year(year).month(month).date(day));
 console.log(currentdayEL);
