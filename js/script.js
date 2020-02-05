d8b7ab5ee2e6869a23cbb6f83842b88a876707d9
Cutie on the Couch <sjpcp5@gmail.com>
2020-02-04 18:29:49 -0600

    added local storage function, getDay moment function works

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 /* alt+shift+a gives me commented paragraphs */
-var currentdayEL = document.getElementById("#currentDay"); 
 var fieldsetEl = document.getElementsByTagName("fieldset");
 var textareaEL = document.getElementsByTagName("textarea");
 var timeblockEl = document.getElementsByTagName("time");
 var inputbtnEl = document.getElementsByTagName("input");
 
-$(currentdayEL).append(moment().year(year).month(month).date(day));
-console.log(currentdayEL);
+function getDay(){
+    return moment().format("MMMM Do YYYY, h:mm:ss a");
+};
+console.log(getDay());
+
+
+ $("#currentDay").text(getDay());
+   
+ function getLocalStorage(){
+     let value = localStorage.getItem();
+     if (value) {
+         $
+     }
+ }
