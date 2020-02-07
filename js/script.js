932ef7717d35667f473b2421d9cb6b6e791a0383
Cutie on the Couch <sjpcp5@gmail.com>
2020-02-06 22:06:34 -0600

    all functions working, need to fix css

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 const fieldsetEl = $("fieldset");
 const textareaEL = $("textarea");
 const timeblockEl = $("time");
-const inputbtnEl = $(".saveBtn b ph3 pv2 input-reset ba f3 bg-light-green");
 
+console.log("consts")
 
 
 // function getDay(){
@@ -11,10 +11,8 @@ const inputbtnEl = $(".saveBtn b ph3 pv2 input-reset ba f3 bg-light-green");
 // };
 // console.log(getDay());
 
-
-
-
 function getLocalStorage(key) {
+    console.log("get local storage")
     let value = localStorage.getItem(key);
     if (value) {
         $(`#text${key}`).text(value);
@@ -24,8 +22,9 @@ const btnInputEl = $("#btnInput");
 const inputEventEl = $("#inputEvent");
 const timeareaEL = $("#time-area");
 const formEL = $("#form")
-
+console.log("2nd consts")
 $(document).ready(function() {
+    console.log("document ready")
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
     for (let i = 9; i < 18; i++) {
 
@@ -33,13 +32,13 @@ $(document).ready(function() {
         var form = $(`<div data-time=${i} id='${i}' class="flex justify-center w-100 pa1">`);
 
         // create a time-area
-        var time = $('<div class="w-25 pa2 mr2 bg-blue"> <time class="hour time-block dib w-100 v-mid tc b:bold f3">' + formatAMPM(i) + '</time>');
+        var time = $('<div class="w-25 pa2 mr2 bg-blue"> <time class="hour ">' + formatAMPM(i) + '</time>');
 
         //create a text button
-        var text = $(`<div class="outline w-100 pa2 mr2" top--1"><textarea id=text${i} class="description dib v-mid w-100 f3" placeholder="Add your event here..."></textarea>`);
+        var text = $(`<div class="outline w-100 pa2 mr2" top--1"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);
 
         //create a button
-        var button = $(`<div class="w-25 pa2 mr3"><input class="saveBtn b ph3 pv2 input-reset ba f3 bg-light-green" type=submit value="Save"  id=${i}></input>`)
+        var button = $(`<div class="w-25 pa2 mr3"><button class="saveBtn" id=${i}><i class="b ph3 pv2 input-reset ba f2 bg-light-green" type=submit value="Save"></input></button>`)
 
         // append col to row
         form.append(time);
@@ -48,11 +47,12 @@ $(document).ready(function() {
 
         // last step add rows to container
         fieldsetEl.append(form);
-
+        console.log("fieldsetEl");
         getLocalStorage(i);
     }
 
     function formatAMPM(hours) {
+        console.log("formateAMPL")
         var ampm = hours >= 12 ? 'pm' : 'am';
         hours = hours % 12;
         hours = hours ? hours : 12;
@@ -61,6 +61,7 @@ $(document).ready(function() {
     formatAMPM();
 
     function updateColors() {
+        console.log("updatedColors")
         var currentTime = new Date().getHours();
         for (var i = 9; i < 18; i++) {
             console.log(currentTime, $(`#${i}`).data("time"));
@@ -70,15 +71,18 @@ $(document).ready(function() {
                 $(`#text${i}`).addClass("future");
             }
         }
-
-        setInterval(function() {
-            updateColors();
-        }, 1000);
     }
-    inputbtnEl.on("click", function() {
-        console.log("i have been clicked");
+    setInterval(function() {
+        console.log("set interval")
+        updateColors();
+    }, 1000);
+
+    const inputbtnEl = $(".saveBtn");
+    inputbtnEl.on('click', function() {
+        console.log("clicked");
         let eventbtnId = $(this).attr('id');
-        let eventText = $(this).parent().siblings().children('.description').value();
+        let eventText = $(this).parent().siblings().children('.description').val();
         localStorage.setItem(eventbtnId, eventText);
-    })
+
+    });
 });
